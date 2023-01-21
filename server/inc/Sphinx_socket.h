162a8ad5207ecf3270112aa5110ec30a504d3ca6
Shinux <shk052353@gmail.com>
2023-01-22 01:41:17 +0900

    [추가] SphinxSocket 추가
    - 서버 개발을 위한 Socket 클래스 생성

---
+++ server/inc/Sphinx_socket.h
@@ -0,0 +1,26 @@
+#ifndef __SPHINX_SOCKET_H__
+#define __SPHINX_SOCKET_H__
+
+namespace sphinx
+{
+
+
+namespace socket
+{
+
+
+class SphinxSocket
+{
+private:
+public:
+private:
+public:
+};
+
+
+}
+
+
+}
+
+#endif
