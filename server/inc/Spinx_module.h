0292ae039223c28741279da7ddf5b8b86f6eaf8c
shinux <shinux@localhost.localdomain>
2023-01-22 01:16:08 +0900

    [수정] 폴더 Server로 변경

--- server/inc/Spinx_module.h
+++ server/inc/Spinx_module.h
@@ -6,6 +6,7 @@
 #include <dlfcn.h>
 
 #include <string>
+#include <unordered_map>
 
 typedef int (*SPINX_MODULE_MAIN)(int, char**);
 typedef void* DLHANDLE;
@@ -19,17 +20,26 @@ namespace module
 class SpinxModuleServer
 {
     // Private Instance
-    DLHANDLE dlHandle;
-    SPINX_MODULE_MAIN moduleFn;
+    std::unordered_map<DLHANDLE, SPINX_MODULE_MAIN> dlMap;
 public:
     // Public Instatnce
 private:
     // Private Method
-    std::string GetSymError(void);
+    std::string 
+    GetSymError(DLHANDLE dlHandle);
 public:
     // Public Method
+
+    ~SpinxModuleServer();
+
     spinx::error::SpinxStatus
-    Commit();
+    Start();
+
+    spinx::error::SpinxStatus
+    Stop();
+
+    spinx::error::SpinxStatus
+    Commit(std::string&& moduleName);
 };
 
 
--- server/src/Spinx_main.cc
+++ server/src/Spinx_main.cc
@@ -4,5 +4,5 @@
 
 int main(int argc, char** argv)
 {
-    spinx::module::SpinxModule spinxModule;
+    spinx::module::SpinxModuleServer spinxModuleServer;
 }
