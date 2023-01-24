6866557980e8f3f9cf6d277dc8c5a40e313725c0
Heisenbug <shk052353@gmail.com>
2023-01-24 16:26:58 +0900

    [추가] SphinxSocket 메소드 추가
    - Create, Bind, Listen관련 메소드 추가

--- server/inc/Sphinx_socket.h
+++ server/inc/Sphinx_socket.h
@@ -1,20 +1,46 @@
 #ifndef __SPHINX_SOCKET_H__
 #define __SPHINX_SOCKET_H__
 
+#include "Sphinx_error.h"
+
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SPHINX_LOCAL_IP 0
+#define SPHINX_PORT     8282
+
 namespace sphinx
 {
 
 
-namespace socket
+namespace Socket
 {
 
 
 class SphinxSocket
 {
 private:
+    int socketFd; // socket
+    int clientFd;
 public:
 private:
+    sphinx::error::SphinxStatus 
+    CreateSocket();
+
+    sphinx::error::SphinxStatus 
+    BindSocket();
+
+    sphinx::error::SphinxStatus
+    ListenSocket();
+
 public:
+    sphinx::error::SphinxStatus 
+    Open();
+
+    sphinx::error::SphinxStatus 
+    Close();
 };
 
 
--- server/src/Sphinx_socket.cc
+++ server/src/Sphinx_socket.cc
@@ -1,1 +1,57 @@
 #include "Sphinx_socket.h"
+
+sphinx::error::SphinxStatus 
+sphinx::Socket::SphinxSocket::CreateSocket()
+{
+    sphinx::error::SphinxStatus status;
+
+    socketFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(socketFd < 0)
+        status = sphinx::error::SphinxErrorSocketCreateFailed();
+    else
+        status = sphinx::error::SphinxOK();
+
+    return status;
+}
+
+sphinx::error::SphinxStatus 
+sphinx::Socket::SphinxSocket::BindSocket()
+{
+    sockaddr_in hostAddr {};
+
+    hostAddr.sin_family = AF_INET;
+    hostAddr.sin_port = htons(SPHINX_PORT);
+    hostAddr.sin_addr.s_addr = 0;
+
+    if(bind(socketFd, reinterpret_cast<sockaddr*>(&hostAddr), sizeof(hostAddr)) < 0);
+        return sphinx::error::SphinxErrorSocketBindFailed();
+}
+
+sphinx::error::SphinxStatus
+sphinx::Socket::SphinxSocket::ListenSocket()
+{
+    if(listen(socketFd, 5) < 0)
+        return sphinx::error::SphinxErrorSocketListenFailed();
+}
+
+sphinx::error::SphinxStatus 
+sphinx::Socket::SphinxSocket::Open()
+{
+    sphinx::error::SphinxStatus status;
+
+    status = CreateSocket();
+    status << "Failed to create Socket!!\n";
+    if(!IS_SPHINX_CLASS_OK(status))
+        return status;
+
+    status = BindSocket();
+    status << "Failed to bind Socket!!\n";
+    if(!IS_SPHINX_CLASS_OK(status))
+        return status;
+}
+
+sphinx::error::SphinxStatus
+sphinx::Socket::SphinxSocket::Close()
+{
+
+}
