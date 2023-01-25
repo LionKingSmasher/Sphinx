bca81b023de332d43dba3db166bb089e9780031c
Heisenbug <shk052353@gmail.com>
2023-01-25 18:25:31 +0900

    [추가] 기본적인 코루틴 구현

--- server/inc/Sphinx_task.h
+++ server/inc/Sphinx_task.h
@@ -9,12 +9,26 @@ namespace sphinx
 namespace task
 {
 
-struct SphinxTask
-{
-    struct SphinxTaskCoroutine
-    {
+struct SphinxTaskPromise;
 
-    };
+struct SphinxTask : std::coroutine_handle<SphinxTaskPromise>
+{
+    using promise_type = struct promise;
+};
+
+struct SphinxTaskPromise
+{
+    SphinxTask 
+    get_return_object() { return {SphinxTask::from_promise(*this)};}
+
+    std::suspend_always 
+    initial_suspend() noexcept { return {}; }
+    
+    std::suspend_always 
+    final_suspend() noexcept { return {}; }
+
+    void return_void() {}
+    void unhandled_exception() {}
 };
 
 }
--- server/src/Sphinx_task.cc
+++ server/src/Sphinx_task.cc
@@ -1,1 +1,2 @@
 #include "Sphinx_task.h"
+
